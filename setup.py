267d4c93e7a3254241d5e40a9073e1bdbae6c794
thehapyone <charlesayibiowu@hotmail.com>
2020-01-12 14:05:40 +0100

    initial changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="nanocamera",
+	version="0.1.0",
+	description="A simple to use camera interface for the Jetson Nano for working cameras in Python.",
+	packages=find_packages(),
+	install_requires=[
+	],
+	)
