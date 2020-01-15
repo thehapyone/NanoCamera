2f3517ff107fe6c2921d9fb7bf31ce008809c9a1
thehapyone <charlesayibiowu@hotmail.com>
2020-01-15 08:27:56 +0100

    updated pip package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
 	name="NanoCamera",
-	version="0.1.0",
+	version="0.1.1",
 	author="Ayo Ayibiowu",
 	author_email="charlesayibiowu@hotmail.com",
 	description="A Python camera interface for the Jetson Nano",
