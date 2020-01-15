de7f34647ca6627cd4015b76c0d78360ffea918e
thehapyone <charlesayibiowu@hotmail.com>
2020-01-15 08:16:18 +0100

    updated pip package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
 	long_description = fh.read()
 
 setup(
-	name="nanocamera",
+	name="NanoCamera",
 	version="0.1.0",
 	author="Ayo Ayibiowu",
 	author_email="charlesayibiowu@hotmail.com",
