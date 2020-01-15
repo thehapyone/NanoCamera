4e4d00c775ba782762da4cb6dc08e38624d10811
thehapyone <charlesayibiowu@hotmail.com>
2020-01-15 08:14:54 +0100

    updated pip package

--- examples/USB_camera.py
+++ examples/USB_camera.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
     # Create the Camera instance
     camera = nano.Camera(camera_type=1, device_id=1, width=640, height=480, fps=30)
-    print('CSI Camera is now ready')
+    print('USB Camera is now ready')
     while True:
         try:
             # read the camera image
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
 	long_description = fh.read()
 
 setup(
-	name="NanoCamera",
+	name="nanocamera",
 	version="0.1.0",
 	author="Ayo Ayibiowu",
 	author_email="charlesayibiowu@hotmail.com",
