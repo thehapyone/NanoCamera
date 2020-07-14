743ded889852a207385536f20e4924a6be235a09
Ayo Ayibiowu <charlesayibiowu@hotmail.com>
2020-07-14 14:03:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 # 
 with open("README.md", "r") as fh:
 	long_description = fh.read()
-	
+
 setup(
 	name="nanocamera",
 	version="0.1.5",
@@ -12,6 +12,11 @@ setup(
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/thehapyone/NanoCamera",
+	project_urls={
+        "Examples": "https://github.com/thehapyone/NanoCamera/tree/master/examples",
+        "Documentation": "https://github.com/thehapyone/NanoCamera/blob/master/README.md",
+        "Source Code": "https://github.com/thehapyone/NanoCamera",
+    },
 	packages=find_packages(),
 	classifiers=[
 		"Programming Language :: Python :: 3",
